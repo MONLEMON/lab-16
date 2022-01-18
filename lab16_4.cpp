1a29278a62781c5f81cc2d7598eb17353884fac5
Surabordin <bluelimec@gmail.com>
2022-01-18 12:41:21 +0700

    lab 16-4 complete

--- lab16_4.cpp
+++ lab16_4.cpp
@@ -17,3 +17,29 @@ int main(){
 	
 	return 0;
 }
+
+void shuffle(int &a,int &b,int &c,int &d){
+	int x[4],y[4];
+	int q=0,w=0,e=0,r=0 ;
+	x[0] = a , x[1] = b,x[2] = c,x[3] = d;
+	q = rand()%4;
+	y[0]=x[q];
+	x[q]=0;
+	do{
+	w = rand()%4;
+	}while (x[w]==0);
+	y[1]=x[w];
+	x[w]=0;
+	do{
+	e = rand()%4;
+	}while (x[e]==0);
+	y[2]=x[e];
+	x[e]=0;
+	do{
+	r = rand()%4;
+	}while (x[r]==0);
+	y[3]=x[r];
+	x[r]=0;
+
+	a = y[0] ,b = y[1],c = y[2],d = y[3];
+}
